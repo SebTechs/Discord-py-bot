faf8f902ef44c4f43006af8f3216e2579e78762c
Jamillll <mjmister2006@outlook.com>
2023-06-29 19:28:31 +0100

    Seb helpppp

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ intents = discord.Intents.all()
 intents.message_content = True
 client = commands.Bot(command_prefix=",", intents= intents)
 
+userIDs = list()
+userXP = list()
+
 @client.event
 async def on_ready(): #This is on startup
     print("Hellow world I am alive now and working maybe Change innit")
@@ -30,8 +33,24 @@ async def help(ctx): #This is on ",help" command
 async def on_message(message): #This is on message
     print(f"message from {message.author}: {message.content}")
     
+    isInList = False
+    for ids in userIDs:
+        if (ids == message.author.id):
+            isInList = True
+
+    if(isInList == False):
+       userIDs.append(message.author.id)
+       print(message.author.id)
+
+    
 @commands.command()
 async def ping(self, ctx): #This is on ",ping" command
     await ctx.send("Pong")
 
-client.run('Token Here please')
+@commands.command(name = "list")
+async def list(self, ctx):
+    for ids in userIDs:
+        await ctx.send(ids)
+        print("AAAA")
+
+client.run('Token')
