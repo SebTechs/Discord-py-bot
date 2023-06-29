69cfeaae8a8e73d4d4650d495fb0337742d0b3dc
Seb <sjalowiecki2@gmail.com>
2023-06-29 17:37:39 +0100

    Initial Commitment

---
+++ main.py
@@ -0,0 +1,32 @@
+#Python  3.11.3
+
+from asyncio import create_task
+import asyncio
+from datetime import datetime
+from operator import truediv
+import discord
+import random
+import os
+from discord import *
+from discord.ext.commands import CommandNotFound, Bot, errors, guild_only
+import time
+from discord.ext import commands 
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix=",", intents= intents)
+@client.event
+async def on_ready():
+    print("Hellow world I am alive now and working maybe")
+    await client.change_presence(activity=discord.Game(name="idk by im doing someting"))
+
+
+#This loads all of the py file (cogs) in ./cogs
+async def load():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await client.load_extension(f'cogs.{filename[:-3]}')
+
+async def main():
+    await load()
+    await client.start('Enter Discord token here.')
+asyncio.run(main())
